9ca9780312e68fb13b1679654d55ecb7818ab285
erlendkvi <erlend.kvisgaard@student.nhh.no>
2023-10-10 11:42:12 +0200

    Finished Problem 2

--- functions/data_transformations.r
+++ functions/data_transformations.r
@@ -4,3 +4,15 @@
 
 # Creating a function to transform the Vegvesenet dataset
 
+transform_metadata_to_df <- 
+  function(md) {
+    md[[1]] %>% 
+      map(as_tibble) %>% 
+      bind_rows() %>% 
+      mutate(latestData = map_chr(latestData, 
+                                  1, 
+                                  .default = NA_character_)) %>% 
+      mutate(latestData = as_datetime(latestData, tz = "Europe/Berlin")) %>% 
+      unnest_wider(location) %>% 
+      unnest_wider(latLon)
+  }
