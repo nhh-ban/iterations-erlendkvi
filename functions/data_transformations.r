f872cc7da7ff3315a0138268fb83f508cd2a6584
erlendkvi <erlend.kvisgaard@student.nhh.no>
2023-10-11 12:20:34 +0200

    Created to_iso8601 function

--- functions/data_transformations.r
+++ functions/data_transformations.r
@@ -3,10 +3,9 @@
 ### Problem 2 ----
 
 # Creating a function to transform the Vegvesenet dataset
-
 transform_metadata_to_df <- 
-  function(md) {
-    md[[1]] %>% 
+  function(df) {
+    df[[1]] %>% 
       map(as_tibble) %>% 
       bind_rows() %>% 
       mutate(latestData = map_chr(latestData, 
@@ -16,3 +15,14 @@ transform_metadata_to_df <-
       unnest_wider(location) %>% 
       unnest_wider(latLon)
   }
+
+# Creating a function to extract an ISO8601 time (with an offset, if applicable)
+to_iso8601 <- 
+  function(dttm, offset_days = 0) {
+    adjusted_for_offset <- as_datetime(dttm) + days(offset_days)
+    adjusted_for_iso <- iso8601(anytime(adjusted_for_offset, tz = "UTC"))
+    return(paste0(adjusted_for_iso, "Z"))
+  }
+
+to_iso8601("2023-10-10 15:20:00") # Testing with today's date
+to_iso8601("2016-09-01 10:11:12", -4) # Testing with an offset
